579fa71b1e5060da4693b7dd348a32aefb26f58b
Y.W KIM <40012548+kyw624@users.noreply.github.com>
2020-06-16 21:13:41 +0900

    Adding a docs

---
+++ basic-sort.py
@@ -0,0 +1,27 @@
+arr = [7, 8, 2, 9, 4, 6]
+
+
+def selection_sort(arr):  # 선택
+    for i in range(len(arr) - 1):
+        min = i
+        for j in range(i + 1, len(arr)):
+            if arr[j] < arr[min]:
+                min = j
+        arr[i], arr[min] = arr[min], arr[i]
+
+
+def insertion_sort(arr):  # 삽입
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+        while (j >= 0 and arr[j] > key):
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+
+
+def bubble_sort(arr):  # 버블
+    for i in range(len(arr) - 1):
+        for j in range(len(arr) - 1 - i):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
