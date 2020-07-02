efa778fec79b6f5523491fe804fbecfc5ca7482f
Y.W KIM <kyw624@gmail.com>
2020-07-02 23:25:48 +0900

    Refactoring

--- BOJ/silver/5585.py
+++ BOJ/silver/5585.py
@@ -2,27 +2,11 @@
 
 price = int(input())
 change = 1000 - price
-coins = 0
+coins = [500, 100, 50, 10, 5, 1]
+cnt = 0
 
+for i in coins:
+    cnt += change // i
+    change %= i
 
-def bill(money, pay, coin):
-    coin += money // pay
-    money %= pay
-    return money, coin
-
-
-while change > 0:
-    if change // 500 >= 1:
-        change, coins = bill(change, 500, coins)
-    elif change // 100 >= 1:
-        change, coins = bill(change, 100, coins)
-    elif change // 50 >= 1:
-        change, coins = bill(change, 50, coins)
-    elif change // 10 >= 1:
-        change, coins = bill(change, 10, coins)
-    elif change // 5 >= 1:
-        change, coins = bill(change, 5, coins)
-    else:
-        change, coins = bill(change, 1, coins)
-
-print(coins)
+print(cnt)
