ba55da49d63a08b538439f122d7fb4a593c209ea
Y.W KIM <kyw624@gmail.com>
2020-08-04 17:03:09 +0900

    Create 10773.py

---
+++ BOJ/silver/10773.py
@@ -0,0 +1,13 @@
+# 제로
+
+K = int(input())
+stack = []
+
+for _ in range(K):
+    num = int(input())
+    if stack and num == 0:
+        stack.pop()
+    else:
+        stack.append(num)
+
+print(sum(stack))
