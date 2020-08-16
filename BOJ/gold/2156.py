5486ee243c29319fdd10360f13c01798ae294311
Y.W KIM <kyw624@gmail.com>
2020-08-16 14:54:42 +0900

    Update 2156.py 포도주 시식_다른 풀이

--- BOJ/gold/2156.py
+++ BOJ/gold/2156.py
@@ -20,3 +20,27 @@ if n > 2:
             dp[i] = dp[i - 3] + arr[i] + arr[i - 1]
 
 print(dp[n])
+
+# 2차원 dp 풀이
+n = int(input())
+arr = [int(input()) for _ in range(n)]
+
+if n == 1:
+    print(arr[0])
+elif n == 2:
+    print(arr[0] + arr[1])
+else:
+    dp = [[0] * 3 for _ in range(n)]
+    _max = 0
+
+    for i in range(n):
+        dp[i][0] = max(dp[i - 1][0], dp[i - 1][1], dp[i - 1][2])
+        dp[i][1] = dp[i - 1][0] + arr[i]
+        dp[i][2] = dp[i - 1][1] + arr[i]
+
+    for i in dp:
+        for j in range(3):
+            if _max < i[j]:
+                _max = i[j]
+
+    print(_max)
