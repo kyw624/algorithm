aa280d88689bb6b67123f452822291058392d961
Y.W KIM <kyw624@gmail.com>
2020-09-23 17:12:25 +0900

    Update 18405.py 경쟁적 전염_리팩토링

--- BOJ/18405.py
+++ BOJ/18405.py
@@ -5,30 +5,19 @@ from collections import deque
 
 def bfs(queue, time):
     while queue:
-        row, col = queue.popleft()
+        virus_type, row, col = queue.popleft()
         if time[row][col] == S:
-            return
-        current = matrix[row][col]
+            break
         for i in range(4):
             nx = row + dx[i]
             ny = col + dy[i]
             if nx < 0 or ny < 0 or nx >= N or ny >= N:
                 continue
             if matrix[nx][ny] == 0:
-                queue.append([nx, ny])
-                matrix[nx][ny] = current
+                queue.append([virus_type, nx, ny])
+                matrix[nx][ny] = virus_type
                 time[nx][ny] = time[row][col] + 1
-
-
-def getSortedVirusCoordinates():
-    array = []
-    # i: 바이러스 번호
-    for i in range(1, K + 1):
-        for row in range(N):
-            for col in range(N):
-                if matrix[row][col] == i:
-                    array.append([row, col])
-    return deque(array)
+    return matrix[X - 1][Y - 1]
 
 
 dx = [-1, 0, 1, 0]
@@ -36,15 +25,19 @@ dy = [0, 1, 0, -1]
 
 N, K = map(int, input().split())
 
-matrix = [list(map(int, input().split())) for _ in range(N)]
-time_table = [[0] * N for _ in range(N)]
+time_table = [[0] * N for _ in range(N)]  # 시간값을 누적 할 2차원 배열
+matrix = []
+virus = []  # 바이러스 종류와 좌표를 받을 배열
 
-# S초 뒤에 (X, Y)의 바이러스 종류 출력
+for i in range(N):
+    matrix.append(list(map(int, input().split())))
+    for j in range(N):
+        if matrix[i][j] != 0:
+            virus.append([matrix[i][j], i, j])
+
+virus.sort()  # 작은 번호부터 정렬
+queue = deque(virus)
+
 S, X, Y = map(int, input().split())
 
-if S > 0:
-    # 낮은 번호의 바이러스 좌표부터 queue에 입력
-    queue = getSortedVirusCoordinates()
-    bfs(queue, time_table)
-
-print(matrix[X - 1][Y - 1])
+print(bfs(queue, time_table))
