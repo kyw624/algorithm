96642fb96a0f736f927f2b8875f23909e08b8ae5
Y.W KIM <kyw624@gmail.com>
2020-11-03 15:20:22 +0900

    Create [백트래킹] 15655.py N과 M (6)

---
+++ BOJ/15655.py
@@ -0,0 +1,23 @@
+# N과 M (6)
+
+
+def solution(position):
+    if position == M:
+        for i in array:
+            print(i, end=" ")
+        print()
+        return
+    for i in range(N):
+        if not isUsed[i] and (not position or LIST[i] >= array[position - 1]):
+            array[position] = LIST[i]
+            isUsed[i] = 1
+            solution(position + 1)
+            isUsed[i] = 0
+
+
+N, M = map(int, input().split())
+LIST = sorted(list(map(int, input().split())))
+isUsed = [0 for _ in range(N)]
+array = [0 for _ in range(M)]
+
+solution(0)
