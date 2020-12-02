5855fb17403f1660b65a5c13517eb484ce1c3ae9
Y.W KIM <kyw624@gmail.com>
2020-12-02 22:47:15 +0900

    Create 자연수 뒤집어 배열로 만들기.js >> 다른 풀이 추가

--- programmers/level1/자연수 뒤집어 배열로 만들기.js
+++ programmers/level1/자연수 뒤집어 배열로 만들기.js
@@ -1,5 +1,20 @@
 // 자연수 n을 뒤집어 각 자릿수를 원소로 가지는 배열 리턴
 
+// 숫자 풀이
+function solution(n) {
+  const answer = [];
+  let copy = n;
+
+  while (copy > 0) {
+    answer.push(copy % 10);
+    copy = Math.floor(copy / 10);
+  }
+
+  return answer;
+}
+
+/*
+// 문자열 풀이
 function solution(n) {
   const answer = [];
   const toString = n + "";
@@ -10,5 +25,6 @@ function solution(n) {
 
   return answer;
 }
+*/
 
 console.log(solution(12345)); // [5, 4, 3, 2, 1]
