21d8b2e50d75fe8ba1747d803d2220bea27a9812
Y.W KIM <kyw624@gmail.com>
2023-05-25 18:55:19 +0900

    Rename: 오래된 python 코드들 분리

---
+++ BOJ/legacy/gold/2240 DP 아직 못품.py
@@ -0,0 +1,19 @@
+# 자두나무
+
+T, W = map(int, input().split())
+
+dp = [[0] * W for _ in range(T)]
+
+dp[0][0] = 1
+
+if i in range(T):
+    tree = int(input())
+    for j in range(W + 1):
+        if j % 2 == 0:
+
+    
+
+'''
+W % 
+dp[T][W] = 
+'''
