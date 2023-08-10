f62884f6974424033fa079a8dd151a318da07a47
Y.W KIM <kyw624@gmail.com>
2023-08-10 16:49:42 +0900

    Refactor: 배열로 구현한 선형 Deque 성능 개선

--- 바킹독/0x07강_덱/js_deque.js
+++ 바킹독/0x07강_덱/js_deque.js
@@ -14,7 +14,12 @@ class Deque {
   }
 
   push_front(num) {
-    this.data = [num].concat(this.data.slice(this.head, this.tail++));
+    if (this.head > 0) {
+      this.data[--this.head] = num;
+    } else {
+      this.data = [num].concat(this.data.slice(this.head, this.tail));
+      this.tail++;
+    }
   }
 
   push_back(num) {
@@ -22,61 +27,26 @@ class Deque {
   }
 
   pop_front() {
-    this.data = this.data.slice(this.head + 1, this.tail--);
+    if (this.isEmpty()) return;
+
+    const popNumber = this.data[this.head++];
+
+    return popNumber;
   }
 
   pop_back() {
-    this.data = this.data.slice(this.head, --this.tail);
+    if (this.isEmpty()) return;
+
+    const popNumber = this.data[--this.tail];
+
+    return popNumber;
   }
 
   front() {
-    return this.data[this.head];
+    return this.isEmpty() ? undefined : this.data[this.head];
   }
 
   back() {
-    return this.data[this.tail - 1];
-  }
-
-  test() {
-    this.push_front(3); // { data: [ 3 ], head: 0, tail: 1 }
-    console.log(dq);
-
-    this.push_front(10); // { data: [ 10, 3 ], head: 0, tail: 2 }
-    console.log(dq);
-
-    this.push_back(5); // { data: [ 10, 3, 5 ], head: 0, tail: 3 }
-    console.log(dq);
-
-    this.push_front(4); // { data: [ 4, 10, 3, 5 ], head: 0, tail: 4 }
-    console.log(dq);
-
-    this.pop_front(); // { data: [ 10, 3, 5 ], head: 0, tail: 3 }
-    console.log(dq);
-
-    this.push_back(50); // { data: [ 10, 3, 5, 50 ], head: 0, tail: 4 }
-    console.log(dq);
-
-    this.pop_back(); // { data: [ 10, 3, 5 ], head: 0, tail: 3 }
-    console.log(dq);
-
-    this.back(); // 5
-    console.log(this.back());
-
-    this.front(); // 10
-    console.log(this.front());
-
-    this.pop_back(); // Deque { data: [ 10, 3 ], head: 0, tail: 2 }
-    console.log(dq);
-
-    this.back(); // 3
-    console.log(this.back());
-
-    this.pop_front(); // Deque { data: [ 3 ], head: 0, tail: 1 }
-    console.log(dq);
-
-    this.front(); // 3
-    console.log(this.front());
-
-    console.log("Result:", dq); // { data: [ 3 ], head: 0, tail: 1 }
+    return this.isEmpty() ? undefined : this.data[this.tail - 1];
   }
 }
